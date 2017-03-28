7373934640506ac094a6aa4f2ff832970de4c43a
Andrew Cahill <andrewsyn@outlook.com>
2017-03-29 00:57:20 +0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services;
+using Actor1.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Actors.Client;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            Task.Run(async () =>
+            {
+                var actorId = ActorId.CreateRandom();
+
+                var simpleActor =
+                ActorProxy.Create<IActor1>(actorId, "fabric:/Application1");
+
+                await simpleActor.SetInputAsync("1+1", new System.Threading.CancellationToken());
+                result = await simpleActor.GetResultAsync(new System.Threading.CancellationToken());
+
+                Console.WriteLine("Result: " + result);
+                Console.ReadLine();
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
